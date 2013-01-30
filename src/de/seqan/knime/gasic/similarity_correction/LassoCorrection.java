c478733ee5bed727558803a4d8b70851dc28dee2
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-01-30 18:50:53 +0100

    added Lasso Regression code similar to the GASiC implementation
    added some test code and data

---
+++ src/de/seqan/knime/gasic/similarity_correction/LassoCorrection.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2006-2013, Knut Reinert, Freie Universitaet Berlin
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Freie Universitaet Berlin nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.seqan.knime.gasic.similarity_correction;
+
+import java.util.Arrays;
+
+import org.ejml.simple.SimpleMatrix;
+
+import com.cureos.numerics.Calcfc;
+import com.cureos.numerics.Cobyla;
+
+/**
+ * Computes the corrected abundance matrix for a given similarity matrix and
+ * read counts.
+ * 
+ * @author aiche
+ */
+public class LassoCorrection {
+
+	// initial value taken from scipy version and GASiC source code
+	private static final double rhobeg = 1.0;
+	private static final double rhoend = 1.0e-10;
+	private static final int iprint = 1;
+	private static final int maxfun = 10000;
+
+	private static double norm(final SimpleMatrix sm) {
+		// we assume that this is a vector
+		double n = 0.0;
+
+		for (int i = 0; i < sm.numRows(); ++i) {
+			n += sm.get(i, 0) * sm.get(i, 0);
+		}
+		return n;
+	}
+
+	/**
+	 * Calculate corrected abundances given a similarity matrix and observations
+	 * using optimization.
+	 * 
+	 * @param similarity
+	 *            Matrix with pairwise similarities between species.
+	 * @param normalizedReadAbundances
+	 *            Vector of read counts per species (normalized).
+	 * @return Estimated abundance of each species in the sample.
+	 */
+	public static double[] similarityCorrection(double[][] similarity,
+			double[] normalizedReadAbundances) {
+
+		final SimpleMatrix sm = new SimpleMatrix(similarity);
+		final SimpleMatrix reads = new SimpleMatrix(sm.numRows(), 1, true,
+				normalizedReadAbundances);
+
+		// compute total number of reads
+		final int numGenoms = reads.numRows();
+
+		// 1 constraint for each read (non-negative) and total sum <= 1
+		final int numConstraints = numGenoms + 1;
+
+		// normalize reads by its total number
+
+		// solve the lasso problem
+		Calcfc calcfc = new Calcfc() {
+			@Override
+			public double Compute(int numVariables, int numConstraints,
+					double[] x, double[] con) {
+
+				// non-negative constraints for all variables
+				for (int i = 0; i < numVariables; ++i) {
+					con[i] = x[i];
+				}
+
+				// sum <= 1 constraint
+				con[numVariables] = 1.0 - sum(x);
+
+				// System.out.println(Arrays.toString(con));
+
+				// convert x to SimpleMatrix
+				SimpleMatrix smX = new SimpleMatrix(sm.numRows(), 1, true, x);
+
+				// compute norm
+				return norm(sm.mult(smX).minus(reads));
+			}
+		};
+
+		// initial guess -> 0.5 for all
+		double[] abbundanceValue = new double[numGenoms];
+		double startParameter = 1 / numGenoms;
+		Arrays.fill(abbundanceValue, startParameter);
+
+		// do the actual optimization
+		Cobyla.FindMinimum(calcfc, numGenoms, numConstraints, abbundanceValue,
+				rhobeg, rhoend, iprint, maxfun);
+
+		return abbundanceValue;
+	}
+
+	private static double sum(double[] x) {
+		double sum = 0.0;
+		for (double d : x) {
+			sum += d;
+		}
+		return sum;
+	}
+}
---
+++ test/de/seqan/knime/gasic/similarity_correction/LassoCorrectionTest.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2006-2013, Knut Reinert, Freie Universitaet Berlin
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Freie Universitaet Berlin nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.seqan.knime.gasic.similarity_correction;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+import org.ejml.simple.SimpleMatrix;
+import org.junit.Test;
+
+/**
+ * @author aiche
+ * 
+ */
+public class LassoCorrectionTest {
+
+	private static final double EPSILON = 0.001;
+
+	private double[][] simpleMatrixTo2DArray(SimpleMatrix sm) {
+		double[][] data = new double[sm.numRows()][sm.numCols()];
+
+		for (int i = 0; i < sm.numRows(); ++i) {
+			for (int j = 0; j < sm.numCols(); ++j) {
+				data[i][j] = sm.get(i, j);
+			}
+		}
+		return data;
+	}
+
+	@Test
+	public void testSimpleMatrixTo2DArray() {
+		double[][] data = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		SimpleMatrix sm = new SimpleMatrix(data);
+
+		assertEquals(1, sm.get(0, 0), EPSILON);
+		assertEquals(2, sm.get(0, 1), EPSILON);
+		assertEquals(3, sm.get(0, 2), EPSILON);
+
+		assertEquals(4, sm.get(1, 0), EPSILON);
+		assertEquals(5, sm.get(1, 1), EPSILON);
+		assertEquals(6, sm.get(1, 2), EPSILON);
+
+		assertEquals(7, sm.get(2, 0), EPSILON);
+		assertEquals(8, sm.get(2, 1), EPSILON);
+		assertEquals(9, sm.get(2, 2), EPSILON);
+
+		double[][] res = simpleMatrixTo2DArray(sm);
+
+		assertEquals(1, res[0][0], EPSILON);
+		assertEquals(2, res[0][1], EPSILON);
+		assertEquals(3, res[0][2], EPSILON);
+
+		assertEquals(4, res[1][0], EPSILON);
+		assertEquals(5, res[1][1], EPSILON);
+		assertEquals(6, res[1][2], EPSILON);
+
+		assertEquals(7, res[2][0], EPSILON);
+		assertEquals(8, res[2][1], EPSILON);
+		assertEquals(9, res[2][2], EPSILON);
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.seqan.knime.gasic.similarity_correction.LassoCorrection#similarityCorrection(double[][], double[])}
+	 * .
+	 * 
+	 * @throws IOException
+	 *             Thrown if loading of matrices fails.
+	 * @throws URISyntaxException
+	 *             Thrown if converting the URI to the test data fails.
+	 */
+	@Test
+	public void testSimilarityCorrection() throws IOException,
+			URISyntaxException {
+
+		final int numReads = 100000;
+
+		String inputFilename = LassoCorrectionTest.class
+				.getResource("input.txt").toURI().getPath();
+		String smMatrixFilename = LassoCorrectionTest.class
+				.getResource("matrix.txt").toURI().getPath();
+		String outputFilename = LassoCorrectionTest.class
+				.getResource("output.txt").toURI().getPath();
+
+		SimpleMatrix input = SimpleMatrix.loadCSV(inputFilename);
+		SimpleMatrix smMatrix = SimpleMatrix.loadCSV(smMatrixFilename);
+		SimpleMatrix output = SimpleMatrix.loadCSV(outputFilename);
+
+		input.print();
+		output.print();
+
+		for (int i = 0; i < input.numCols(); ++i) {
+			System.out.println("Test iteration: " + i);
+
+			SimpleMatrix currentInput = input.extractVector(false, i);
+			SimpleMatrix currentOutput = output.extractVector(false, i);
+
+			// currentInput.print();
+			SimpleMatrix normalizedInput = currentInput.divide(numReads);
+			// normalizedInput.print();
+
+			// currentOutput.print();
+
+			double[] corrected = LassoCorrection.similarityCorrection(
+					simpleMatrixTo2DArray(smMatrix),
+					normalizedInput.getMatrix().data);
+
+			// assertArrayEquals(currentOutput.getMatrix().getData(), corrected,
+			// EPSILON);
+
+			System.out.println("Result:   " + Arrays.toString(corrected));
+			System.out.println("Expected: "
+					+ Arrays.toString(currentOutput.getMatrix().getData()));
+			double[] diff = new double[corrected.length];
+			for (int d = 0; d < corrected.length; ++d) {
+				diff[d] = Math.abs(corrected[d]
+						- currentOutput.getMatrix().getData()[d]);
+			}
+			System.out.println("Diff:     " + Arrays.toString(diff));
+		}
+	}
+}
